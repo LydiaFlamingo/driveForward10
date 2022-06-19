c56c7ec58ef6af0e7126e2923569d22f075edc49
cannotilever <cannotilever@protonmail.com>
2022-06-18 20:52:40 -0400

    initial setup

---
+++ driveForward10.py
@@ -0,0 +1,7 @@
+import libhousy
+#You can define helper functions here, make sure to but them *above* the main function
+def main(robot: libhousy.robot):
+    #Here is where your recurring code will go
+    print("Hello World!")
+    # This tells the robot we're done and it can move on
+    return libhousy.DONE
